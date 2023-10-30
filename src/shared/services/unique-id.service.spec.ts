0ac4aa5348cf8b229e8a5152b25c24e35cc88ece
Edson Carlos <edsoncarlos@ymail.com>
2023-10-30 00:55:53 -0300

    test: expectativa com contexto

--- src/shared/services/unique-id.service.spec.ts
+++ src/shared/services/unique-id.service.spec.ts
@@ -38,10 +38,10 @@ describe(UniqueIdService.name, () => {
   })
 
   it(`#${UniqueIdService.prototype.generatedUniqueIdWithPrefix.name} should throw when called with empty}`, () => {
-    const emptyValues = [null, undefined, ''];
+    const emptyValues = [null, undefined, '', '0', '1', 'app'];
 
     emptyValues.forEach(emptyValue => {
-      expect(() => service.generatedUniqueIdWithPrefix(emptyValue)).toThrow();
+      expect(() => service.generatedUniqueIdWithPrefix(emptyValue)).withContext(`Empty Value: ${emptyValue}`).toThrow();
     })
   })
 });
