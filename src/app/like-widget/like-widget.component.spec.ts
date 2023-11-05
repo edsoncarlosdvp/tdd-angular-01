ff46c96aa3e32707c164a7169bd8e570596f9151
Edson Carlos <edsoncarlos@ymail.com>
2023-11-05 19:59:45 -0300

    test: Criando instâncias de componentes

--- src/app/like-widget/like-widget.component.spec.ts
+++ src/app/like-widget/like-widget.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LikeWidgetComponent } from './like-widget.component';
+import { UniqueIdService } from 'src/shared/services/unique-id.service';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 describe('LikeWidgetComponent', () => {
   let component: LikeWidgetComponent;
@@ -8,7 +10,9 @@ describe('LikeWidgetComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [LikeWidgetComponent]
+      declarations: [LikeWidgetComponent],
+      providers: [UniqueIdService],
+      imports: [FontAwesomeModule]
     });
     fixture = TestBed.createComponent(LikeWidgetComponent);
     component = fixture.componentInstance;
